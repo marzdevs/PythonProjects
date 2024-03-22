b034bec419705847c74b4eba1cd705b0b7306943
Marzdevs <alzokarimarwa@gmail.com>
2024-03-22 10:15:07 -0500

    web scrapper final

---
+++ ExpenseTracker/ExpenseTracker.py
@@ -0,0 +1,8 @@
+import pandas as pd                       #to perform data manipulation and analysis
+import numpy as np                        #to cleanse data
+from datetime import datetime             #to manipulate dates
+import plotly.express as px               #to create interactive charts
+import plotly.graph_objects as go         #to create interactive charts
+from jupyter_dash import JupyterDash      #to build Dash apps from Jupyter environments
+import dash_core_components as dcc        #to get components for interactive user interfaces
+import dash_html_components as html       #to compose the dash layout using Python structures
---
+++ password_manager.py
@@ -0,0 +1,48 @@
+from cryptography.fernet import Fernet #encrypts text
+
+'''
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+'''
+
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+
+master_pwd = input("What is the master password? ")
+key = load_key() +  master_pwd.bytes
+fer = Fernet(key)
+
+def view():
+    with open('passwords.txt', 'r') as f:
+        for line in f.readlines():
+            data = line.rstrip() # rstrip lets you read without the newline at end
+            user, passw = data.split("|")
+            print("User:", user, "| Password:", passw)
+
+def add():
+    name = input('Account Name: ')
+    pwd = input("Password: ")
+
+    with open('passwords.txt', 'a') as f:
+        f.write(name + "|" + pwd + "\n")
+
+
+
+while True:
+    mode = input("Would you like to add a new password or view existing ones (view, add), press q to quit? ").lower()
+    if mode == "q":
+        break
+
+    if mode == "view":
+        view()
+    elif mode == "add":
+        add()
+    else:
+        print("Invalid mode.")
+        continue
